906fe571d0a34f4ea904095ae9f6599a3b50f76c
ShakingDonut <kacper.siembab@gmail.com>
2024-06-19 13:34:37 +0200

    add links to navbar

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -1,10 +1,17 @@
+import { Stack } from "@mui/material";
 import styles from "./style.module.css";
 import UserButton from "./user";
+import { Link } from "react-router-dom";
 
 export default function Navbar() {
     return (
         <nav className={styles.wrapper}>
             <h1>Library system</h1>
+            <Stack direction={"row"} spacing={2} alignItems={"center"}>
+                <Link to={"/"}>Home</Link>
+                <Link to={"/books"}>Books</Link>
+                <Link to={"/login"}>Login</Link>
+            </Stack>
             <UserButton />
         </nav>
     )
